76f8fb835adc2f256b775edc65901b4df19b4819
Walter Berggren <walter.berggren@gmail.com>
2018-01-04 18:39:21 +0530

    Add endpoint /cities for retrieving cities with their attributes

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.create([
+    { name: 'Helsinki', latitude: 60.1697530, longitude: 24.9490830 },
+    { name: 'Tokyo', latitude: 35.6584421, longitude: 139.7328635 },
+    { name: 'New York', latitude: 40.7406905, longitude: -73.9938438 },
+    { name: 'Amsterdam', latitude: 52.3650691, longitude: 4.9040238 },
+    { name: 'Dubai', latitude: 25.092535, longitude: 55.1562243 }
+])
