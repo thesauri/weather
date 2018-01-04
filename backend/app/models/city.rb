96250119a21d4b8a73c8837ee99c36c31dc7fd6f
Walter Berggren <walter.berggren@gmail.com>
2018-01-04 19:15:06 +0530

    Return lo, hi, and temp with their created_at details

--- backend/app/models/city.rb
+++ backend/app/models/city.rb
@@ -1,11 +1,15 @@
 class City < ApplicationRecord
     has_many :measurements
 
+    def temperature
+        self.measurements.last
+    end
+
     def low
-        self.measurements.where(created_at: (Time.now - 24.hours)..Time.now)&.minimum(:temperature) 
+        self.measurements.where(created_at: (Time.now - 24.hours)..Time.now)&.order(:temperature).first 
     end
 
     def high
-        self.measurements.where(created_at: (Time.now - 24.hours)..Time.now)&.maximum(:temperature) 
+        self.measurements.where(created_at: (Time.now - 24.hours)..Time.now)&.order(:temperature).last
     end
 end
--- backend/app/serializers/city_serializer.rb
+++ backend/app/serializers/city_serializer.rb
@@ -1,3 +1,3 @@
 class CitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :latitude, :longitude, :low, :high
+  attributes :id, :name, :latitude, :longitude, :low, :high, :temperature
 end
