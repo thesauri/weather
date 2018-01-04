4a6ea5f3b27dde388495c7059eec815638ed371c
Walter Berggren <walter.berggren@gmail.com>
2018-01-04 19:43:47 +0530

    Fetch city data and display it

--- frontend/src/CityCard.js
+++ frontend/src/CityCard.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 
 class CityCard extends Component {
   render() {
+    const tempText = this.props.temperature ? `${this.props.temperature}°C` : "—";
+    const lowHighText = this.props.low ?
+      `Today's high was ${this.props.high}°C and the low was ${this.props.low}°C` :
+      "No data from the last 24 hours";
     return (
       <div className="card is-inline-block">
           <div className="card-content">
             <div className="media">
               <div className="media-left">
-                  <h1 className="is-title">-10°C</h1>
+                  <h1 className="is-title">
+                     { tempText }
+                  </h1>
               </div>
               <div className="media-content">
-                  <p className="title is-4">Helsinki</p>
+                  <p className="title is-4">{this.props.name}</p>
               </div>
             </div>
             <div className="content">
-              <p>Today's high was -5°C and the low was -15°C</p>
+              <p>{ lowHighText }</p>
               <p>Last update: 5 minutes ago</p>
             </div>
           </div>
@@ -28,7 +34,11 @@ class CityCard extends Component {
 }
 
 CityCard.propTypes = {
-  onCityUpdate: PropTypes.func.isRequired
+  onCityUpdate: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  temperature: PropTypes.string,
+  low: PropTypes.string,
+  high: PropTypes.string
 }
 
 
--- frontend/src/CityList.js
+++ frontend/src/CityList.js
@@ -3,12 +3,38 @@ import PropTypes from 'prop-types';
 import CityCard from './CityCard';
 
 class CityList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/cities")
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({
+          cities: result.data
+        });
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    const cityCards = this.state.cities.map((city) => (
+      <CityCard
+        key={city.id}
+        onCityUpdate={() => this.props.onCityUpdate(city.id)}
+        name={city.attributes.name}
+        temperature={city.attributes.temperature && city.attributes.temperature.temperature}
+        low={city.attributes.low && city.attributes.low.temperature}
+        high={city.attributes.high && city.attributes.high.temperature} />
+    ));
     return (
       <div>
-        <CityCard onCityUpdate={this.props.onCityUpdate} />
-        <CityCard onCityUpdate={this.props.onCityUpdate} />
-        <CityCard onCityUpdate={this.props.onCityUpdate} />
+        {cityCards}
       </div>
     );
   }
